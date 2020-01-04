6a234918f185656e833ec3295f8ee6ac9ff2626c
Joshua Vaughn <j.vaughn.dev@gmail.com>
2020-01-03 22:04:28 -0700

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Jen Larissa",
+  shortName: "Jen Larissa",
+  description:
+    "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Imed Adel",
+  twitter: "Imed_Adel",
+}
